94b75d999ce43796d0a989f69cb886290bb33e59
Matteo Santoro <101145328+ZetaTeo@users.noreply.github.com>
2025-06-05 17:10:02 +0200

    restore

--- Universita/src/main/java/zteo/esercitazione/Universita/entity/enumeration/CorsoDiLaurea.java
+++ Universita/src/main/java/zteo/esercitazione/Universita/entity/enumeration/CorsoDiLaurea.java
@@ -1,8 +1,5 @@
 package zteo.esercitazione.Universita.entity.enumeration;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonValue;
-import zteo.esercitazione.Universita.exception.HandleInvalidEnumValue;
 
 public enum CorsoDiLaurea {
     COMPUTER_SCIENCES,
@@ -12,33 +9,3 @@ public enum CorsoDiLaurea {
 }
 
 
-//public enum CorsoDiLaurea {
-//    DATA_SCIENCE("Scienze e Tecnologie Informatiche"),
-//    MATHEMATICAL_SCIENCES("Scienze Matematiche");
-//
-//    private final String nome;
-//
-//    CorsoDiLaurea(String nome)
-//    {
-//        this.nome = nome;
-//    }
-//
-//    @JsonValue
-//    public String getNomeCorsoDiLaurea()
-//    {
-//        return nome;
-//    }
-//
-//    @JsonCreator
-//    public static CorsoDiLaurea fromNome(String input)
-//    {
-//        for(CorsoDiLaurea corso : values())
-//        {
-//            if(corso.getNomeCorsoDiLaurea().equalsIgnoreCase(input))
-//            {
-//                return corso;
-//            }
-//        }
-//        throw new HandleInvalidEnumValue(input);
-//    }
-//}
